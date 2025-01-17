483ce0531b025a48f1034eca613b1af17a343bc3
Sofia Berdichevska <sofiaxberdichevska@gmail.com>
2025-01-17 13:12:07 +0100

    Updates DB

--- HotelDB.sql
+++ HotelDB.sql
@@ -1,3 +1,29 @@
+BEGIN;
+
+CREATE TABLE Dienstleistung (
+    ServiceID INT PRIMARY KEY,
+    Bezeichnung VARCHAR(50) CHECK (Bezeichnung IN ('Spa', 'Restaurant', 'Unterhaltung')),
+    Preis DECIMAL(10, 2) CHECK (Preis >= 0),
+    Beschreibung TEXT
+);
+
+CREATE TABLE Reservierung (
+    ReservierungsID INT PRIMARY KEY,
+    Zimmernummer INT, -- Fremdschlüssel zur Tabelle Zimmer
+    Gesamtsumme DECIMAL(10, 2) CHECK (Gesamtsumme >= 0),
+    Buchungsstatus VARCHAR(50) NOT NULL,
+    Check_out DATE NOT NULL, 
+    Check_in DATE NOT NULL,
+    CONSTRAINT FK_Zimmer FOREIGN KEY (Zimmernummer) REFERENCES Zimmer(Zimmernummer)
+);
+
+CREATE TABLE Zimmer (
+    Zimmernummer INT PRIMARY KEY,
+    PreisProNacht DECIMAL(10, 2) CHECK (PreisProNacht >= 0),
+    Verfuegbarkeit BOOLEAN NOT NULL,
+    Kategorie VARCHAR(50) CHECK (Kategorie IN ('Einzelzimmer', 'Doppelzimmer', 'Suite'))
+);
+
 CREATE TABLE Gast (
     GastID INT PRIMARY KEY,
     ReservierungsID INT, -- Fremdschlüssel zur Tabelle Reservierung
@@ -6,41 +32,27 @@ CREATE TABLE Gast (
     Geburtsdatum DATE NOT NULL,
     Telefonnummer VARCHAR(20),
     Email VARCHAR(100) NOT NULL,
-    Adresse VARCHAR(100) NOT NULL
-)
+    Adresse VARCHAR(100) NOT NULL,
+    CONSTRAINT FK_Reservierung FOREIGN KEY (ReservierungsID) REFERENCES Reservierung(ReservierungsID)
+);
 
 CREATE TABLE Rechnung (
     RechnungsID INT PRIMARY KEY,
     GastID INT, -- Fremdschlüssel zur Tabelle Gast
     Zahlungsart VARCHAR(50) NOT NULL,
     Zahlungsdatum DATE NOT NULL,
-    Betrag DECIMAL(10, 2) CHECK (Betrag >= 0)
-)
+    Betrag DECIMAL(10, 2) CHECK (Betrag >= 0),
+    CONSTRAINT FK_Gast_Rechnung FOREIGN KEY (GastID) REFERENCES Gast(GastID)
+);
 
 CREATE TABLE Feedback (
     FeedbackID INT PRIMARY KEY,
     GastID INT, -- Fremdschlüssel zur Tabelle Gast
     Bewertung INT CHECK (Bewertung >= 1 AND Bewertung <= 5),
     Datum DATE NOT NULL,
-    Kommentar TEXT
-)
-
-CREATE TABLE Reservierung (
-    ReservierungsID INT PRIMARY KEY,
-    Zimmernummer INT, -- Fremdschlüssel zur Tabelle Zimmer
-    Gesamtsumme DECIMAL(10, 2) CHECK (Gesamtsumme >= 0),
-    Buchungsstatus VARCHAR(50) NOT NULL,
-    Check_out DATE NOT NULL, 
-    Check_in DATE NOT NULL
-)
-
-CREATE TABLE Zimmer (
-    Zimmernummer INT PRIMARY KEY,
-    PreisProNacht DECIMAL(10, 2) CHECK (PreisProNacht >= 0),
-    Kategorie VARCHAR(50) NOT NULL,
-    Verfuegbarkeit BOOLEAN NOT NULL,
-    Kategorie VARCHAR(50) CHECK (Kategorie IN ('Einzelzimmer', 'Doppelzimmer', 'Suite'))
-)
+    Kommentar TEXT,
+    CONSTRAINT FK_Gast FOREIGN KEY (GastID) REFERENCES Gast(GastID)
+);
 
 CREATE TABLE Personal (
     MitarbeiterID INT PRIMARY KEY,
@@ -49,22 +61,15 @@ CREATE TABLE Personal (
     Position VARCHAR(50) NOT NULL,
     Vorname VARCHAR(50) NOT NULL,
     Nachname VARCHAR(50) NOT NULL
-)
-
-CREATE TABLE Dienstleistung (
-    ServiceID INT PRIMARY KEY,
-    Bezeichnung VARCHAR(50) CHECK (Bezeichnung IN ('Spa', 'Restaurant', 'Unterhaltung')),
-    Preis DECIMAL(10, 2) CHECK (Preis >= 0),
-    Beschreibung TEXT
-)
+);
 
 CREATE TABLE Spa (
     ServiceID INT, -- Fremdschlüssel zur Tabelle Dienstleistung
     Oeffnungszeiten VARCHAR(50) NOT NULL,
     Ausstattung TEXT,
     Kapazitaet INT CHECK (Kapazitaet >= 0),
-    CONSTRAINT FK_Dienstleistung FOREIGN KEY (ServiceID) REFERENCES Dienstleistung(ServiceID)
-)
+    CONSTRAINT FK_Spa_Dienstleistung FOREIGN KEY (ServiceID) REFERENCES Dienstleistung(ServiceID)
+);
 
 CREATE TABLE Unterhaltung (
     ServiceID INT, -- Fremdschlüssel zur Tabelle Dienstleistung
@@ -73,8 +78,8 @@ CREATE TABLE Unterhaltung (
     Datum DATE NOT NULL,
     Art VARCHAR(50) CHECK (Art IN ('Konzert', 'Theater', 'Film')),
     Beschreibung TEXT,
-    CONSTRAINT FK_Dienstleistung FOREIGN KEY (ServiceID) REFERENCES Dienstleistung(ServiceID)
-)
+    CONSTRAINT FK_Unterhaltung_Dienstleistung FOREIGN KEY (ServiceID) REFERENCES Dienstleistung(ServiceID)
+);
 
 CREATE TABLE Restaurant (
     ServiceID INT, -- Fremdschlüssel zur Tabelle Dienstleistung
@@ -82,8 +87,10 @@ CREATE TABLE Restaurant (
     Oeffnungszeiten VARCHAR(50) NOT NULL,
     Speisen TEXT,
     Getraenke TEXT,
-    CONSTRAINT FK_Dienstleistung FOREIGN KEY (ServiceID) REFERENCES Dienstleistung(ServiceID)
-)
+    CONSTRAINT FK_Restaurant_Dienstleistung FOREIGN KEY (ServiceID) REFERENCES Dienstleistung(ServiceID)
+);
+
+COMMIT;
 
 
 
